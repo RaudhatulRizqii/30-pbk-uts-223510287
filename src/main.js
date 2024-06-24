cdd20400df5b57bd33e006648f1e1586ff43078e
RaudhatulRizqii <168437957+RaudhatulRizqii@users.noreply.github.com>
2024-06-24 14:44:47 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import "./assets/main.css";
 
-createApp(App).mount('#app')
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+
+import "@quasar/extras/material-icons/material-icons.css";
+
+import "quasar/src/css/index.sass";
+import App from "./App.vue";
+import router from "./router";
+
+const app = createApp(App);
+const pinia = createPinia();
+
+app.use(Quasar, {
+  plugins: {}, 
+});
+
+app.use(pinia);
+app.use(router);
+
+// Mount the app
+app.mount("#app");
